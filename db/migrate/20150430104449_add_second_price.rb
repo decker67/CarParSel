bcdce3b25f71161bdf8db1cd952cc4bf51c57010
Dirk Ecker <dirk.ecker@gmx.net>
2015-05-01 21:49:27 +0200

    field type of second price changed to decimal

--- db/migrate/20150430104449_add_second_price.rb
+++ db/migrate/20150430104449_add_second_price.rb
@@ -1,5 +1,5 @@
 class AddSecondPrice < ActiveRecord::Migration
   def change
-    add_column :car_parts, :second_price, :number, precision: 18, scale: 2
+    add_column :car_parts, :second_price, :decimal, precision: 18, scale: 2
   end
 end
