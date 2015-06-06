2e2f25a844d7209e289e1af4bfbef6837f100df1
Dirk Ecker <dirk.ecker@gmx.net>
2015-06-06 14:20:30 +0200

    fix

--- db/migrate/20150605115316_add_state_to_car_parts.rb
+++ db/migrate/20150605115316_add_state_to_car_parts.rb
@@ -1,6 +1,6 @@
 class AddStateToCarParts < ActiveRecord::Migration
   def change
-    add_column :car_parts, :state, :integer
-    CarPart.update_all(state: 3000) #set state to 3000 for all existing entries (Gebraucht)
+    add_column :car_parts, :state1, :integer
+    CarPart.update_all(state1: 3000) #set state to 3000 for all existing entries (Gebraucht)
   end
 end
