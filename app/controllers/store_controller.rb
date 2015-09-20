b7bd1873e5e456684fd4032a8ce30e8c48853b22
Dirk Ecker <dirk.ecker@gmx.net>
2015-09-20 19:33:29 +0200

    scanning for parts completed

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,11 +3,35 @@ class StoreController < ApplicationController
   layout 'mobile'
 
   def index
+  end
 
+  def store_part
+    result = {}
+    storageId = params[:storage]
+    carPartId = params[:part]
+    storage = nil
+    car_part = nil
+    storageInfo = 'unbekannt'
+    carPartInfo = 'unbekannt'
+
+    begin
+      storage = Storage.find(storageId)
+      storageInfo = storage.name
+      car_part = CarPart.find(carPartId)
+      carPartInfo = car_part.description
+      car_part.update(storage_id: storage.id)
+    rescue ActiveRecord::RecordNotFound
+    ensure
+      render json: {
+          car_part: carPartInfo,
+          storage: storageInfo
+      }
+    end
   end
 
   def search
 
   end
+
 
 end
